1e2b07757f1c8bfc782cdcc10db88906caf0625f
Mohammed27s <mohammed.23s@outlook.com>
2024-05-02 17:46:21 +0400

    The snippet code for Item is complete

---
+++ Item Interface.java
@@ -0,0 +1,13 @@
+package Item;
+//This is Item behaviours
+public interface Item {
+    
+    void itemId();
+    void itemName();
+    void itemPrice();
+    void quantity();
+    void total();
+    void tax();
+    
+    
+}
